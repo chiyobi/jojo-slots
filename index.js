5ddaad5ccc60dc5a3169f03ac8957a87e2ac5dd6
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-03-03 15:04:32 -0800

    feat: show choices before landing on randomly chosen
    feat: show choices before landing on randomly chosen
    feat: play song after landing on chosen

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 const slotMachine = document.getElementById("slot-machine");
-const lim = 10
-let entry;
-const yellow = '#fcba03';
-const blue = '#0040ff';
-const red = '#ff0048';
+const aud = document.querySelector("audio");
+
+const yellow = '#f2bf30';
+const blue = '#3d64d9';
+const red = '#eb4473';
 const colors = [yellow, red, blue];
+let entry;
 let colorIndex = 0;
-for (let i = 1; i <= lim; i++) {
-  const img = new Image();
-  img.src = `./public/images/${i}.png`;
-  img.alt = `Jojo pose ${i}`;
-  entry = document.createElement("li");
-  entry.classList.add("entry");
-
-  const color = colors[colorIndex];
+let color;
+let img;
+const images = [];
+const n = 48;
+for (let i = 0; i < n; i++) {
+  img = new Image();
+  img.src = `./public/images/${i % n}.png`;
+  img.alt = `Jojo pose ${i % n}`;
+  color = colors[colorIndex];
+  
   colorIndex = (colorIndex + 1) % colors.length;
-
-  entry.style.border = `.5rem solid ${color}`;
-  entry.append(img);
-  slotMachine.append(entry);
+  img.style.border = `.4rem solid ${color}`;
+  images.push(img);
 }
 
 function spin() {
-  slotMachine.style.transform = `translateX(-100px)`; 
+  aud.pause();
+  aud.currentTime = 0;
+
+  const randIndex = Math.floor(Math.random() * n) + 10;
+  let i = 0;
+  const intervalId = setInterval(function() {
+
+    if (i === randIndex) {
+      clearInterval(intervalId);
+      aud.play();
+    }
+
+    const entry = slotMachine.querySelector("img");
+    entry.replaceWith(images[i % n]);
+    i++;
+
+  }, 100);
 }
+
+function init() {
+  const index = Math.floor(Math.random() * n);
+  const image = images[index];
+  slotMachine.append(image);
+}
+
+init();
