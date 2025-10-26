30b2c7ca1f1c5006d2ae9afd49c06d1cd4f6b3c0
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-10-25 22:40:04 -0700

    feat: add recent section and enlarge window

--- index.js
+++ index.js
@@ -19,28 +19,57 @@ for (let i = 0; i < n; i++) {
   color = colors[colorIndex];
   
   colorIndex = (colorIndex + 1) % colors.length;
-  img.style.border = `.4rem solid ${color}`;
+  img.style.border = `.4em solid ${color}`;
   images.push(img);
 }
 
+const zoomEl = document.querySelector('#zoom');
+const zoomImgContainerEl = document.querySelector('#zoom .img-container');
+const closeZoom = () => {
+  zoomEl.style.height = '0px';
+  zoomImgContainerEl.innerHTML = null;
+}
+const zoom = (el) => {
+  zoomImgContainerEl.append(el);
+  zoomEl.style.height = '100vh';
+}
+
+const recentContainer = document.querySelector('#recent-images');
+recentContainer.addEventListener('click', function(e) {
+  const el = e.target;
+  zoom(el.cloneNode());
+})
+
+const seen = new Set();
 function spin() {
   aud.pause();
   aud.currentTime = 0;
 
   const randIndex = (startIndex + Math.floor(Math.random() * n))%n;
   let i = startIndex;
+  let selectedImage = null;
+  
   const intervalId = setInterval(function() {
+    let currentIndex = i%n;
 
-    if (i%n === randIndex) {
-      clearInterval(intervalId);
+    if (currentIndex === randIndex) {
       aud.play();
+      
+      selectedImage = images[currentIndex].cloneNode();
+      if (!seen.has(currentIndex)) {
+        seen.add(currentIndex);
+        recentContainer.append(selectedImage);
+      }
+      
+      clearInterval(intervalId);
     }
 
     const entry = slotMachine.querySelector("img");
-    entry.replaceWith(images[i % n]);
+    entry.replaceWith(images[currentIndex]);
     i++;
-
+    
   }, 100);
+
 }
 
 function init() {
